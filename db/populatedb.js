85fa5d99a03d64d507fcbf625e6d084c43e15ccb
jdogcodey <jonnydawson@hotmail.com>
2024-12-19 20:53:59 +0000

    Update script to populate db correctly

--- db/populatedb.js
+++ db/populatedb.js
@@ -7,22 +7,23 @@ const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 
 const SQL = `
-CREATE TABLE IF NOT EXISTS usernames (
+CREATE TABLE IF NOT EXISTS messages (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  username VARCHAR ( 255 )
+  user VARCHAR ( 255 ),
+  text VARCHAR ( 255 ), 
+  added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO usernames (username) 
+INSERT INTO messages (text, user) 
 VALUES
-  ('Bryan'),
-  ('Odin'),
-  ('Damon');
+  ('Amando', 'Hi there!'),
+  ('Charles', 'Hi world!');
 `;
 
 async function main() {
   console.log("seeding...");
   const client = new Client({
-    connectionString: `postgresql://${user}:${password}@localhost:5432/top_users`,
+    connectionString: `postgresql://${user}:${password}@localhost:5432/mini-message`,
   });
   await client.connect();
   await client.query(SQL);
